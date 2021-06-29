4cc77f0eda3e9927c705b1948664d62944e6e1b1
legg2710 <luisemilio2710@gmail.com>
2021-06-29 00:10:09 +0000

    To Do List Fetch 50%

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,63 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+import React, { useState } from "react";
 export function Home() {
+	const [todos, setTodos] = useState([]);
+	const [task, setTask] = useState("");
+	function todosEliminar(index) {
+		if (index > -1) {
+			console.log("eliminar");
+			const filterList = todos.filter(item => item !== todos[index]);
+			setTodos(filterList);
+		}
+	}
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center mt-5 container">
+			<h1 className="display-4">To Do List</h1>
+
+			<form
+				onSubmit={evento => {
+					evento.preventDefault();
+					//para setear el enter solo cuando exista texto en el input
+					if (task.length > 0) setTodos([...todos, task]);
+					setTask("");
+				}}>
+				<div className="form-group">
+					<input
+						type="text"
+						className="form-control"
+						placeholder="What needs to be done?"
+						onChange={evento => setTask(evento.target.value)}
+						value={task}></input>
+					<div className="form-group-append"></div>
+				</div>
+			</form>
+			{/* 04 mostrar el contenido del arreglo */}
+			<ul className="list-group list-group-flush">
+				{todos.map((item, index) => {
+					return (
+						<li className="list-group-item" key={index}>
+							<span>{item}</span>
+							<button
+								className="btn btn-light float-right"
+								onClick={
+									() => todosEliminar(index)
+									// index,
+									// "contenido:",
+									// todos[index]
+								}>
+								<i
+									className="fa fa-trash"
+									aria-hidden="true"></i>
+							</button>
+						</li>
+					);
+				})}
+			</ul>
+			<div>
+				<p className="text-muted float-left">
+					{todos.length}
+					{""} item left
+				</p>
+			</div>
 		</div>
 	);
 }
