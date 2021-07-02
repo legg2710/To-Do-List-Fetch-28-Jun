700d4d21fe7ae13cbf03cf2cfc8ef983ba5d89d0
legg2710 <luisemilio2710@gmail.com>
2021-07-02 05:13:19 +0000

    To Do List Fetch: Data Guardada en useState pt2

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
 export function Home() {
-	//01 - Crear un array
 	const [todos, setTodos] = useState([]);
-	//02 - Guardar texto que digita el usuario
+
 	const [task, setTask] = useState("");
-	//03 - Crear funcion que llamara al api
+
 	const todoReq = () => {
 		fetch("https://assets.breatheco.de/apis/fake/todos/user/legg2710", {
 			method: "GET",
@@ -14,26 +13,29 @@ export function Home() {
 				"Content-Type": "application/json"
 			}
 		})
-			//Cuando la api conteste el llamado entra el metodo .then() que retorna una promesa.
-			//En este caso pedimos el parametro resp.
-			.then(resp => {
-				console.log(resp.ok); // si el llamado es exitoso el response retornara true.
-				console.log(resp.status); // retornara el estado del code = 200 or code = 400 etc.
-				console.log(resp.text()); // retornara el response exacto como string.
-				return resp.json(); // retorna la promesa del api y la convierte a json (este es un proceso asincrono)
-			})
-			//El parametro data retorna la info de la api
-			.then(data => {
-				//here is were your code should start after the fetch finishes
-				console.log(data); //aqui imprime el objeto llamado del servidor (api)
-				//04. modificamos la variable de estado
-				setTodos();
-				//se ha guardado la data en el useState
-			})
-			.catch(error => {
-				//error handling
-				console.log(error);
-			});
+			.then(resp => resp.json())
+			.then(data => setTodos(data));
+
+		// //Cuando la api conteste el llamado entra el metodo .then() que retorna una promesa.
+		// //En este caso pedimos el parametro resp.
+		// .then(resp => {
+		// 	console.log(resp.ok); // si el llamado es exitoso el response retornara true.
+		// 	console.log(resp.status); // retornara el estado del code = 200 or code = 400 etc.
+		// 	console.log(resp.text()); // retornara el response exacto como string.
+		// 	return resp.json(); // retorna la promesa del api y la convierte a json (este es un proceso asincrono)
+		// })
+		// //El parametro data retorna la info de la api
+		// .then(data => {
+		// 	//here is were your code should start after the fetch finishes
+		// 	console.log(data); //aqui imprime el objeto llamado del servidor (api)
+		// 	//04. modificamos la variable de estado
+		// 	setTodos();
+		// 	//se ha guardado la data en el useState
+		// })
+		// .catch(error => {
+		// 	//error handling
+		// 	console.log(error);
+		// });
 	};
 
 	function todosEliminar(index) {
@@ -43,6 +45,11 @@ export function Home() {
 			setTodos(filterList);
 		}
 	}
+	///5. Ejecutamos el useEffect al final, justo antes del return, llamando la funcion que contiene el GET
+	useEffect(() => {
+		todoReq();
+	}, []);
+
 	return (
 		<div className="text-center mt-5 container">
 			<h1 className="display-4">To Do List</h1>
