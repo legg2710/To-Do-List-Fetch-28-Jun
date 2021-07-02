6368b6e1c1b70251ffed3a053a73e070b2dabb99
legg2710 <luisemilio2710@gmail.com>
2021-07-02 03:35:35 +0000

    To Do List Fetch: Data Guardada en useState

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,7 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 export function Home() {
+	//01 - Crear un array
 	const [todos, setTodos] = useState([]);
+	//02 - Guardar texto que digita el usuario
 	const [task, setTask] = useState("");
+	//03 - Crear funcion que llamara al api
+	const todoReq = () => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/legg2710", {
+			method: "GET",
+			// body: JSON.stringify(todos),  //El body se usa cuando creamos o modificamos algo.
+			//Los headers establecen el lenguaje de la comunicacion entre el front y la api.
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			//Cuando la api conteste el llamado entra el metodo .then() que retorna una promesa.
+			//En este caso pedimos el parametro resp.
+			.then(resp => {
+				console.log(resp.ok); // si el llamado es exitoso el response retornara true.
+				console.log(resp.status); // retornara el estado del code = 200 or code = 400 etc.
+				console.log(resp.text()); // retornara el response exacto como string.
+				return resp.json(); // retorna la promesa del api y la convierte a json (este es un proceso asincrono)
+			})
+			//El parametro data retorna la info de la api
+			.then(data => {
+				//here is were your code should start after the fetch finishes
+				console.log(data); //aqui imprime el objeto llamado del servidor (api)
+				//04. modificamos la variable de estado
+				setTodos();
+				//se ha guardado la data en el useState
+			})
+			.catch(error => {
+				//error handling
+				console.log(error);
+			});
+	};
+
 	function todosEliminar(index) {
 		if (index > -1) {
 			console.log("eliminar");
